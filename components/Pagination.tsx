c7870f1fd4a7c7e2c5d74abde425d13c11c97743
DESKTOP-L8P1VHU\neron <neronim66@gmail.com>
2022-06-27 17:49:24 +0200

    changed pagination to use file name

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -7,7 +7,7 @@ interface PageLinkProps {
 const PageLink = ({ current = false, page }: PageLinkProps) => {
   return (
     <a
-      href={`/products?page=${page}`}
+      href={`/products/${page}`}
       className={`border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium
     ${current ? "border-indigo-500 text-indigo-600" : ""}`}
     >
