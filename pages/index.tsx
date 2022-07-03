1cd2421eacdb3cd79e2df09d1c8b0b7591c05742
DESKTOP-L8P1VHU\neron <neronim66@gmail.com>
2022-07-03 15:58:10 +0200

    removed old data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,4 @@
 import { Main } from "../components/Main";
-import { ProductDetails } from "../components/Product";
-
-const DATA = {
-  thumbnailUrl: "https://picsum.photos/id/1060/536/354",
-  thumbnailAlt: "Barista",
-  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-  explicabo deleniti asperiores autem facere atque reprehenderit ipsam
-  magni veritatis beatae, laboriosam, magnam exercitationem perspiciatis
-  quae doloremque! Eveniet iste dicta quasi?`,
-  rating: 4.5,
-};
 
 const Home = () => {
   return <Main>Hi</Main>;
